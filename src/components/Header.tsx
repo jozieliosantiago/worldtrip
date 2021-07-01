ce8b469146dfb57ef796b3be6f4038105c6accc8
Joziélio Santiago <jozieliosantiago@gmail.com>
2021-06-30 22:40:14 -0300

    header responsivity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,17 @@
 import { Flex, Image } from '@chakra-ui/react';
+import Link from 'next/link';
 
 export function Header() {
   return (
     <Flex w="100%" as="header" h="8vh" p="20px" justify="center" align="center">
-      <Image src="./images/logo.svg" alt="logo" />
+      <Link href="/" passHref>
+        <Image
+          w={['120px', '200px']}
+          cursor="pointer"
+          src="./images/logo.svg"
+          alt="logo"
+        />
+      </Link>
     </Flex>
   );
 }
